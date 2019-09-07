6e002e301ef944d975544f7be8a615658ed21a77
juhengze <18653858844@163.com>
2019-09-08 00:32:03 +0800

    antd配置完成，按需加载

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import {Button} from "antd"
-import 'antd/dist/antd.css'
+import {Button} from "antd";
 import './App.css';
-import { from } from 'rxjs';
 
 function App() {
   return (
     <div className="App">
-      <Button>antd</Button>
+      <Button type="primary">Primary</Button>
     </div>
   );
 }
